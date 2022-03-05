c31be49f98899c3ed1c8da3f2c78068ce58d7562
mnikv92 <nikmarasanov@gmail.com>
2022-03-05 13:03:22 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
     selector: 'my-app',
-    template: `<child-comp></child-comp>
-                <p>Привет {{name}}</p>`,
-    styles: [`h2, p {color:#333;}`]
+    styles: [`
+        input.ng-touched.ng-invalid {border:solid red 2px;}
+        input.ng-touched.ng-valid {border:solid green 2px;}
+    `],
+    template: `<form #myForm="ngForm" novalidate>
+                    <div class="form-group">
+                        <label>Имя</label>
+                        <input class="form-control" name="name" [(ngModel)]="name" required />
+                    </div>
+                    <div class="form-group">
+                        <label>Email</label>
+                        <input class="form-control" name="email" ngModel required email />
+                    </div>
+                    <div class="form-group">
+                        <label>Телефон</label>
+                        <input class="form-control" name="phone" ngModel required pattern="[0-9]{10}" />
+                    </div>
+                    <div class="form-group">
+                        <button [disabled]="myForm.invalid"
+                                class="btn btn-default" (click)="submit(myForm)">Добавить</button>
+                    </div>
+                </form>
+                <div>Имя: {{myForm.value.name}}</div>
+                <div>Email: {{myForm.value.email}}</div>
+                <div>Номер: {{myForm.value.phone}}</div>`
 })
 export class AppComponent {
-    name = 'Петр';
+
+    name: string = "";
+    email: string = "";
+    phone: string = "";
+
+    submit(myForm: NgForm) {
+        console.log(myForm);
+    }
 }
